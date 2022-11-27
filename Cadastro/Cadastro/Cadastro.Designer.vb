3b9bea17e377ff5003d9a563e8c8be5fff5effe0
Victor Mariani <54690428+vmm2136@users.noreply.github.com>
2022-11-27 16:12:59 -0300

    Cadastro de Alunos
    Projeto finalizado de acordo com as especificações requisitadas. Especificações disponíveis no readme

---
+++ Cadastro/Cadastro/Cadastro.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Cadastro
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CodAluno = New System.Windows.Forms.TextBox()
+        Me.Nome = New System.Windows.Forms.TextBox()
+        Me.Idade = New System.Windows.Forms.TextBox()
+        Me.Cidade = New System.Windows.Forms.TextBox()
+        Me.dgvDados = New System.Windows.Forms.DataGridView()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Escolaridade = New System.Windows.Forms.ComboBox()
+        Me.Estado = New System.Windows.Forms.ComboBox()
+        Me.btnCadastrar = New System.Windows.Forms.Button()
+        Me.btnExcluir = New System.Windows.Forms.Button()
+        Me.btnConsulta = New System.Windows.Forms.Button()
+        Me.btnAtualizar = New System.Windows.Forms.Button()
+        Me.Search = New System.Windows.Forms.TextBox()
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CodAluno
+        '
+        Me.CodAluno.Location = New System.Drawing.Point(186, 46)
+        Me.CodAluno.Name = "CodAluno"
+        Me.CodAluno.Size = New System.Drawing.Size(121, 20)
+        Me.CodAluno.TabIndex = 0
+        '
+        'Nome
+        '
+        Me.Nome.Location = New System.Drawing.Point(186, 79)
+        Me.Nome.Name = "Nome"
+        Me.Nome.Size = New System.Drawing.Size(121, 20)
+        Me.Nome.TabIndex = 1
+        '
+        'Idade
+        '
+        Me.Idade.Location = New System.Drawing.Point(186, 116)
+        Me.Idade.Name = "Idade"
+        Me.Idade.Size = New System.Drawing.Size(121, 20)
+        Me.Idade.TabIndex = 2
+        '
+        'Cidade
+        '
+        Me.Cidade.Location = New System.Drawing.Point(186, 153)
+        Me.Cidade.Name = "Cidade"
+        Me.Cidade.Size = New System.Drawing.Size(121, 20)
+        Me.Cidade.TabIndex = 3
+        '
+        'dgvDados
+        '
+        Me.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDados.Location = New System.Drawing.Point(347, 41)
+        Me.dgvDados.Name = "dgvDados"
+        Me.dgvDados.Size = New System.Drawing.Size(343, 225)
+        Me.dgvDados.TabIndex = 11
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(36, 41)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(126, 23)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "Código do Aluno"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(36, 76)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(52, 23)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Nome"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(36, 111)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(49, 23)
+        Me.Label11.TabIndex = 9
+        Me.Label11.Text = "Idade"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(36, 148)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(60, 23)
+        Me.Label12.TabIndex = 10
+        Me.Label12.Text = "Cidade"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(36, 183)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(58, 23)
+        Me.Label13.TabIndex = 11
+        Me.Label13.Text = "Estado"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(36, 218)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(101, 23)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Escolaridade"
+        '
+        'Escolaridade
+        '
+        Me.Escolaridade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Escolaridade.FormattingEnabled = True
+        Me.Escolaridade.Location = New System.Drawing.Point(186, 223)
+        Me.Escolaridade.Name = "Escolaridade"
+        Me.Escolaridade.Size = New System.Drawing.Size(121, 21)
+        Me.Escolaridade.TabIndex = 5
+        '
+        'Estado
+        '
+        Me.Estado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Estado.FormattingEnabled = True
+        Me.Estado.Location = New System.Drawing.Point(186, 185)
+        Me.Estado.Name = "Estado"
+        Me.Estado.Size = New System.Drawing.Size(121, 21)
+        Me.Estado.TabIndex = 4
+        '
+        'btnCadastrar
+        '
+        Me.btnCadastrar.Location = New System.Drawing.Point(40, 271)
+        Me.btnCadastrar.Name = "btnCadastrar"
+        Me.btnCadastrar.Size = New System.Drawing.Size(97, 23)
+        Me.btnCadastrar.TabIndex = 6
+        Me.btnCadastrar.Text = "Cadastrar Aluno"
+        Me.btnCadastrar.UseVisualStyleBackColor = True
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.Location = New System.Drawing.Point(382, 364)
+        Me.btnExcluir.Name = "btnExcluir"
+        Me.btnExcluir.Size = New System.Drawing.Size(97, 23)
+        Me.btnExcluir.TabIndex = 10
+        Me.btnExcluir.Text = "Excluir Aluno"
+        Me.btnExcluir.UseVisualStyleBackColor = True
+        '
+        'btnConsulta
+        '
+        Me.btnConsulta.Location = New System.Drawing.Point(40, 364)
+        Me.btnConsulta.Name = "btnConsulta"
+        Me.btnConsulta.Size = New System.Drawing.Size(97, 23)
+        Me.btnConsulta.TabIndex = 8
+        Me.btnConsulta.Text = "Consultar Aluno"
+        Me.btnConsulta.UseVisualStyleBackColor = True
+        '
+        'btnAtualizar
+        '
+        Me.btnAtualizar.Location = New System.Drawing.Point(188, 271)
+        Me.btnAtualizar.Name = "btnAtualizar"
+        Me.btnAtualizar.Size = New System.Drawing.Size(119, 23)
+        Me.btnAtualizar.TabIndex = 7
+        Me.btnAtualizar.Text = "Atualizar Cadastro"
+        Me.btnAtualizar.UseVisualStyleBackColor = True
+        '
+        'Search
+        '
+        Me.Search.Location = New System.Drawing.Point(154, 367)
+        Me.Search.Name = "Search"
+        Me.Search.Size = New System.Drawing.Size(176, 20)
+        Me.Search.TabIndex = 9
+        '
+        'Cadastro
+        '
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(702, 413)
+        Me.Controls.Add(Me.Search)
+        Me.Controls.Add(Me.btnAtualizar)
+        Me.Controls.Add(Me.btnConsulta)
+        Me.Controls.Add(Me.btnExcluir)
+        Me.Controls.Add(Me.btnCadastrar)
+        Me.Controls.Add(Me.Estado)
+        Me.Controls.Add(Me.Escolaridade)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.dgvDados)
+        Me.Controls.Add(Me.Cidade)
+        Me.Controls.Add(Me.Idade)
+        Me.Controls.Add(Me.Nome)
+        Me.Controls.Add(Me.CodAluno)
+        Me.Name = "Cadastro"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Alunos"
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+
+    Friend WithEvents CodAluno As TextBox
+    Friend WithEvents Nome As TextBox
+    Friend WithEvents Idade As TextBox
+    Friend WithEvents Cidade As TextBox
+    Friend WithEvents dgvDados As DataGridView
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Escolaridade As ComboBox
+    Friend WithEvents Estado As ComboBox
+    Friend WithEvents btnCadastrar As Button
+    Friend WithEvents btnExcluir As Button
+    Friend WithEvents btnConsulta As Button
+    Friend WithEvents btnAtualizar As Button
+    Friend WithEvents Search As TextBox
+End Class
---
+++ Cadastro/Cadastro/Cadastro.vb
@@ -0,0 +1,219 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Cadastro
+
+    'Realiza a conexão com o BD
+    Dim con As New SqlConnection(" Server = DESKTOP-FGL20RU\SQLEXPRESS01 ;Database=Alunos ;User Id= sa; Password=123;")
+
+
+    Private Sub validarCamposPreenchidos()
+
+        'Valida os campos do formulário. Primeiro valida se os campos estão preenchidos e depois valida se o código do aluno ou o nome ja estão cadastrados.
+        If Not IsNumeric(CodAluno.Text) Then
+
+            MsgBox("É necessário informar o código do aluno!", MsgBoxStyle.Exclamation)
+            CodAluno.SelectAll()
+            CodAluno.Focus()
+
+        ElseIf Nome.Text = "" Then
+
+            MsgBox("É necessário informar o nome do aluno!", MsgBoxStyle.Exclamation)
+            Nome.SelectAll()
+            Nome.Focus()
+
+        ElseIf Not IsNumeric(Idade.Text) Then
+
+            MsgBox("É necessário informar a idade do aluno!", MsgBoxStyle.Exclamation)
+            Idade.SelectAll()
+            Idade.Focus()
+
+        ElseIf Cidade.Text = "" Then
+
+            MsgBox("É necessário informar a cidade do aluno!", MsgBoxStyle.Exclamation)
+            Cidade.SelectAll()
+            Cidade.Focus()
+        ElseIf CodAluno IsNot Nothing Then
+
+            Dim cod As Integer = CodAluno.Text
+            con.Open()
+            Dim validaCod As New SqlCommand("select count(CodAluno) from CadAlunos where CodAluno = '" & cod & "'", con)
+            Dim existeCod As Integer = validaCod.ExecuteScalar()
+
+            If existeCod > 0 Then
+
+                MsgBox("Código ja cadastrado!! ", MsgBoxStyle.Exclamation)
+
+            ElseIf Nome.Text <> "" Then
+
+                Dim nomeAluno As String = Nome.Text
+                Dim validaNome As New SqlCommand("select count(nome) from CadAlunos where Nome = '" & nomeAluno & "'", con)
+                Dim existeNome As Integer = validaNome.ExecuteScalar()
+
+                If existeNome > 0 Then
+
+                    MsgBox("Nome ja cadastrado!! ", MsgBoxStyle.Exclamation)
+
+                Else
+
+                    cadastraAluno()
+
+                End If
+                con.Close()
+            End If
+            con.Close()
+        End If
+
+
+    End Sub
+
+    Public Sub limpaCampos()
+
+        'Limpa os campos após realizar a inclusão, alteração ou exclusão.
+
+        CodAluno.Clear()
+        Nome.Clear()
+        Idade.Clear()
+        Cidade.Clear()
+
+    End Sub
+
+    Private Sub cadastraAluno()
+
+        'Realiza o cadastro do aluno no banco de dados, caso os dados passem nas validações.
+
+        Dim cod As Integer = CodAluno.Text
+        Dim nomeAluno As String = Nome.Text
+        Dim idadeAluno As Integer = Idade.Text
+        Dim cidadeAluno As String = Cidade.Text
+        Dim estadoAluno As String = Estado.Text
+        Dim escolaridadeAluno As String = Escolaridade.Text
+
+        Dim command As New SqlCommand("insert into CadAlunos values('" & cod & "','" & nomeAluno & "' , '" & idadeAluno & "','" & cidadeAluno & "','" & estadoAluno & "','" & escolaridadeAluno & "' )", con)
+
+        command.ExecuteNonQuery()
+        MsgBox("Aluno Cadastrado Com Sucesso!", MsgBoxStyle.Information)
+        LoadDataInGrid()
+        con.Close()
+
+    End Sub
+
+    Private Sub btnCadastrar_Click(sender As Object, e As EventArgs) Handles btnCadastrar.Click
+
+        'O botão de cadastrar quando 'pressionado' chama o procedimento validarCamposPreenchidos que por sua vez chama o cadastraAluno.
+        validarCamposPreenchidos()
+
+        limpaCampos()
+
+    End Sub
+
+    Private Sub LoadDataInGrid()
+
+        'Exibe os dados ao lado do cadastro e os atualiza conforme as operações são realizadas.
+
+        Dim command As New SqlCommand("select CodAluno, Nome, Idade from CadAlunos", con)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        dgvDados.DataSource = dt
+
+    End Sub
+
+    Private Sub Cadastro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        LoadDataInGrid()
+
+        'Busca os estados no BD para retorna-los na ComboBox "Estado"
+
+        Dim command As New SqlCommand("select * from Estados", con)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable
+        adapter.Fill(table)
+        Estado.DataSource = table
+        Estado.DisplayMember = "Estado"
+        Estado.ValueMember = "Id"
+
+        'Busca os estados no BD para retorna-los na ComboBox "Escolaridade"
+
+        Dim command2 As New SqlCommand("select * from Escolaridade", con)
+        Dim adapter2 As New SqlDataAdapter(command2)
+        Dim table2 As New DataTable
+        adapter2.Fill(table2)
+        Escolaridade.DataSource = table2
+        Escolaridade.DisplayMember = "Escolaridade"
+        Escolaridade.ValueMember = "Id"
+    End Sub
+
+    Private Sub btnAtualizar_Click(sender As Object, e As EventArgs) Handles btnAtualizar.Click
+
+        'Realiza a alteração do cadastro com base no código do aluno
+
+        If Not IsNumeric(CodAluno.Text) Then
+
+            MsgBox("É necessário informar o código do aluno para realizar a alteração!", MsgBoxStyle.Exclamation)
+
+        Else
+
+            Dim cod As Integer = CodAluno.Text
+            Dim nomeAluno As String = Nome.Text
+            Dim idadeAluno As Integer = Idade.Text
+            Dim cidadeAluno As String = Cidade.Text
+            Dim estadoAluno As String = Estado.Text
+            Dim escolaridadeAluno As String = Escolaridade.Text
+
+            con.Open()
+            Dim command As New SqlCommand("update CadAlunos set Nome = '" & nomeAluno & "' , Idade =  '" & idadeAluno & "', Cidade = '" & cidadeAluno & "', Estado = '" & estadoAluno & "',Escolaridade = '" & escolaridadeAluno & "' where  CodAluno = '" & cod & "'", con)
+            command.ExecuteNonQuery()
+            con.Close()
+            MsgBox("Aluno Alterado Com Sucesso!", MsgBoxStyle.Information)
+            LoadDataInGrid()
+
+            limpaCampos()
+        End If
+    End Sub
+
+
+    Private Sub btnExcluir_Click(sender As Object, e As EventArgs) Handles btnExcluir.Click
+
+        'Realiza a exclusão do cadastro específicado pelo Código do aluno - Botão Excluir
+
+        If Not IsNumeric(CodAluno.Text) Then
+
+            MsgBox("É necessário informar o código do aluno para realizar a exclusão!", MsgBoxStyle.Exclamation)
+
+        Else
+
+            Dim cod As Integer = CodAluno.Text
+            Dim takeName As New SqlCommand("Select Nome From CadAlunos where CodAluno = '" & cod & "'", con)
+            con.Open()
+            Dim nomeAluno As String = takeName.ExecuteScalar().ToString()
+
+            If MsgBox("Tem Certeza que deseja excluir o aluno " & cod & "-" & nomeAluno & "", MsgBoxStyle.YesNo) = DialogResult.Yes Then
+
+                Dim command As New SqlCommand("delete CadAlunos where CodAluno = '" & cod & "'", con)
+                command.ExecuteNonQuery()
+                MsgBox("Aluno Excluído Com Sucesso!", MsgBoxStyle.Information)
+                con.Close()
+                LoadDataInGrid()
+
+            End If
+            con.Close()
+        End If
+
+        limpaCampos()
+    End Sub
+
+    Private Sub btnConsulta_Click(sender As Object, e As EventArgs) Handles btnConsulta.Click
+
+        'Realiza a consulta do aluno pelo valor informado no campo "Search", seja este valor referente ao nome, idade ou código do aluno - Botão Consultar Aluno
+
+        Dim referencia As String = Search.Text
+        Dim command1 As New SqlCommand("select CodAluno, nome, idade from CadAlunos where Nome like  '%" & referencia & "%' or CodAluno like '" & referencia & "%' or Idade like '" & referencia & "%'", con)
+        Dim sda1 As New SqlDataAdapter(command1)
+        Dim dt1 As New DataTable
+        sda1.Fill(dt1)
+        dgvDados.DataSource = dt1
+
+    End Sub
+
+
+End Class
